35156c86740acd94328b54bcb7bf3d9191c85cc9
nickolayArbuzov <nickarbuzov@yandex.by>
2021-06-16 00:14:02 +0300

    feat: add model user

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log('server')
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+
+const app = express();
+const PORT = config.get("serverPort");
+
+const start = async () => {
+    try {
+
+        mongoose.connect(config.get("dbURL"))
+        app.listen(PORT, () => {
+            console.log('serverStart')
+        })
+    } catch (e) {
+
+    }
+}
+
+start()
